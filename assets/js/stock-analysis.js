c44b4d8bafa0619af348b0f83a22c7dc9ba53c65
singla1209 <arvind.singla12@gmail.com>
2026-08-11 00:07:50 +0530

    Update stock-analysis.js

--- assets/js/stock-analysis.js
+++ assets/js/stock-analysis.js
@@ -939,7 +939,7 @@ const resultsWithFactors =
 const rankedResults =
     rankStocksByFactorScore(resultsWithFactors);
 
-     
+    console.log("ONE RANKED STOCK:", rankedResults[0]); 
 
 renderFactorRanking(rankedResults);
 
